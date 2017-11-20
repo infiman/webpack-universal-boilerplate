8bf78039c1b0b9dad9b38c9a999a21476b600c03
Caseyyu2 <caseyyu2@gmail.com>
2017-11-19 22:38:24 -0800

    chaining not working

--- src/client/App/index.jsx
+++ src/client/App/index.jsx
@@ -1,26 +1,54 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { renderRoutes } from 'react-router-config';
+import GSAP from 'react-gsap-enhancer';
+import { TweenMax, TimelineMax } from 'gsap';
 
-import './assets/index.scss';
+function createAnim() {
+  const box = document.getElementById('box');
+
+  const timelineM = new TimelineMax(box);
+  // TODO: why not chaining, why why why!
+  const tween1 = TweenMax.to(box, 1, { scale: 1.5, y: '+120', yoyo: true });
+  const tween2 = TweenMax.to(box, 1, { scale: 1, y: '-120', yoyo: true });
+  timelineM.repeat(-1);
+  timelineM.add([tween1, tween2], 0.5);
+  return timelineM;
+}
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { x: 300, y: 300 };
+  }
+
+  componentDidMount() {
+    this.jumpAnim = this.addAnimation(createAnim);
+  }
+
   render() {
+    // TODO: utilize incorporating state into animation
+    const { x, y } = this.state;
+
+    const style = {
+      backgroundColor: 'grey',
+      width: 123,
+      height: 123,
+    };
+
+    const containerStyle = {
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      top: x,
+      left: y,
+    };
+
     return (
-      <section>
-        {`Yey! I can be reloaded! Right now I'm on "${this.props.router.location.pathname}"`}
-        {renderRoutes(this.props.route.routes)}
-      </section>
+      <div style={containerStyle}>
+        <div id={'box'} style={style} />
+      </div>
     );
   }
 }
 
-function mapStateToProps({ router }) {
-  return {
-    router,
-  };
-}
-
-const ConnectedApp = connect(mapStateToProps, () => ({}))(App);
-
+const ConnectedApp = GSAP()(App);
 export default ConnectedApp;
